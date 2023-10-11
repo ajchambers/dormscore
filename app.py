995728ea052b283f224a75fb401f4aa3f39fd727
Louis Jankowski <42618080+5498Louis@users.noreply.github.com>
2023-10-11 18:24:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/index')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
