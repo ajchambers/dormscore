eb7c10a011aea4a2ab06ef8f8afbbebf9a581ddb
bowma2s <bowma2s@cmich.edu>
2023-11-03 14:50:22 -0400

    redoing of file structure, updates to html

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template
+import psycopg2
+import psycopg2.extras
+
+from fun import fetch_all_dorms, fetch_dorms_by_university
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def default():
+def home():
     return render_template('home.html')
 
 @app.route('/index')
-
 def index():
     return render_template('index.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/schools')
+def schools():
+    dorms_cmich = fetch_dorms_by_university('Central Michigan University')
+    all_dorms = fetch_all_dorms()
+    return render_template('schools.html', dorms_cmich=dorms_cmich, all_dorms=all_dorms)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- fun.py
+++ fun.py
@@ -1,10 +1,12 @@
+from jinja2 import Environment, FileSystemLoader
 import psycopg2
 import psycopg2.extras
 
 conn = None
 cur = None
 
-try:
+def fetch_dorms_by_university(university):
+    # Fetch data from PostgreSQL and return it
     conn = psycopg2.connect(
         host='localhost',
         dbname='dormscore_v1',
@@ -12,65 +14,23 @@ try:
         password='coffee',
         port=5432
     )
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT dorm_name, address FROM dorm WHERE university = %s", (university,))
+    dorms = cur.fetchall()
+    conn.close()
+    return dorms
 
-    def create_dorm_table(conn):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute('drop table if exists dorm')
-
-        create_script = ''' create table if not exists dorm(
-                            id  serial primary key,
-                            dorm_name text not null,
-                            university text not null,
-                            address text,
-                            avg_score int,
-                            url text,
-                            number_of_reviews int)'''
-
-        cur.execute(create_script)
-
-    def insert_dorm_records(conn, records):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        insert_script = 'INSERT INTO dorm (dorm_name, university, address, avg_score, url, number_of_reviews) VALUES(%s, %s, %s, %s, %s, %s)'
-
-        for record in records:
-            cur.execute(insert_script, record)
-
-    def fetch_dorms_by_university(conn, university):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("SELECT dorm_name, address FROM dorm WHERE university = %s", (university,))
-        return cur.fetchall()
-
-    def fetch_all_dorms(conn):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("SELECT * FROM dorm")
-        return cur.fetchall()
-
-    create_dorm_table(conn)
-
-    insert_values = [
-        ('emmons', 'Central Michigan University', '500 Ojibway Ct, Mt Pleasant, MI 48858', 0, 'https://www.cmich.edu/student-life/housing/living-on-campus/housing-communities/east-community', 0),
-        ('herring', 'Central Michigan University', '403 E Broomfield St, Mt Pleasant, MI 48858', 0, 'https://www.cmich.edu/student-life/housing/living-on-campus/housing-communities/east-community', 0),
-        ('saxe', 'Central Michigan University', 'Saxe Hall, Mt Pleasant, MI 48858', 0, 'https://www.cmich.edu/student-life/housing/living-on-campus/housing-communities/east-community', 1)
-    ]
-
-    insert_dorm_records(conn, insert_values)
-
-    university = 'Central Michigan University'
-    dorms = fetch_dorms_by_university(conn, university)
-    for record in dorms:
-        print(record)
-
-    all_dorms = fetch_all_dorms(conn)
-    for record in all_dorms:
-        print(record['dorm_name'], record['avg_score'])
-
-    conn.commit()
-
-except Exception as error:
-    print(error)
-
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+def fetch_all_dorms():
+    # Fetch data from PostgreSQL and return it
+    conn = psycopg2.connect(
+        host='localhost',
+        dbname='dormscore_v1',
+        user='postgres',
+        password='coffee',
+        port=5432
+    )
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT dorm_name, avg_score FROM dorm")
+    all_dorms = cur.fetchall()
+    conn.close()
+    return all_dorms
